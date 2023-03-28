329e9f12c14f6ea7e8e515bb1c35c47394d6b736
luiz.zanoni <luiz.zanoni@CLOVIS>
2023-03-28 09:26:41 -0300

    realize sua pergunta

--- basico.py
+++ basico.py
@@ -3,5 +3,5 @@ import openai
 openai.api_key = "##Sua api com a openai##"
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
-                                          messages=[{"role": "user", "content": "quanto é 2 + 2?"}])
+                                          messages=[{"role": "user", "content": "#ESCREVA SUA PERGUNTA AQUI#"}])
 print(completion.choices[0].message.content)
